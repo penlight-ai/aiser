38faec848883f101a84dfb419e3ec171910bb793
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-15 01:31:33 +0300

    Move FastAPI app definition inside SimpleAiServer

--- aiser/ai_server/simple_ai_server.py
+++ aiser/ai_server/simple_ai_server.py
@@ -4,26 +4,26 @@ from fastapi.responses import StreamingResponse
 from .ai_server import AiServer
 import asyncio
 
-app = FastAPI()
-
-
-@app.get("/knowledge-base/{kb_id}")
-async def knowledge_base(kb_id: str):
-    return {"message": "Hello World"}
-
-
-async def stream_hello_world():
-    message = 'hello world'
-    for character in message:
-        await asyncio.sleep(0.1)
-        yield character
-
-
-@app.get("/agent/{agent_id}/chat")
-async def agent(agent_id: str):
-    return StreamingResponse(stream_hello_world(), media_type="text/event-stream")
-
 
 class SimpleAiServer(AiServer):
+    def _get_app(self) -> FastAPI:
+        app = FastAPI()
+
+        @app.get("/knowledge-base/{kb_id}")
+        async def knowledge_base(kb_id: str):
+            return {"message": "Hello World"}
+
+        async def stream_hello_world():
+            message = 'hello world'
+            for character in message:
+                await asyncio.sleep(0.1)
+                yield character
+
+        @app.get("/agent/{agent_id}/chat")
+        async def agent(agent_id: str):
+            return StreamingResponse(stream_hello_world(), media_type="text/event-stream")
+
+        return app
+
     def run(self):
-        uvicorn.run(app=app, port=self._port)
+        uvicorn.run(app=self._get_app(), port=self._port)
