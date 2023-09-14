cb5aabf0ed3f9eae8e06cc4f4af986be6a33347c
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-14 18:57:10 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiapi',
+    version='0.1.0',
+    url='https://github.com/penlight-ai/aiapi',
+    author='Penlight AI Inc.',
+    author_email='support@penlight.ai',
+    description='Python package to serve AI agents and knowledge bases for Penlight AI.',
+    packages=find_packages(),
+    install_requires=[],
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License'
+        'Programming Language :: Python :: 3.11',
+    ],
+)
