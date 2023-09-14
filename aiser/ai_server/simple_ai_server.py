cf25117d992fb4b688777e7a656ae9d91edc519d
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-15 01:29:29 +0300

    Add an agent chat endpoint that streams 'hello world'

--- aiser/ai_server/simple_ai_server.py
+++ aiser/ai_server/simple_ai_server.py
@@ -1,14 +1,27 @@
-from .ai_server import AiServer
 import uvicorn
-
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from .ai_server import AiServer
+import asyncio
 
 app = FastAPI()
 
 
 @app.get("/knowledge-base/{kb_id}")
-async def root(kb_id: str):
+async def knowledge_base(kb_id: str):
     return {"message": "Hello World"}
+
+
+async def stream_hello_world():
+    message = 'hello world'
+    for character in message:
+        await asyncio.sleep(0.1)
+        yield character
+
+
+@app.get("/agent/{agent_id}/chat")
+async def agent(agent_id: str):
+    return StreamingResponse(stream_hello_world(), media_type="text/event-stream")
 
 
 class SimpleAiServer(AiServer):
