dc830aff94ae52bfce2919a0d88e3bde69df1c05
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-14 19:33:22 +0300

    Rename to ServAI

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# AI API
+# ServAI
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aiapi',
+    name='servai',
     version='0.1.0',
     url='https://github.com/penlight-ai/aiapi',
     author='Penlight AI Inc.',
