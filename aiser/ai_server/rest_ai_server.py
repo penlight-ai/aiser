f295c975d3e0c7a94272fe0b47b68065cfb7c793
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-17 02:28:42 +0300

    Use two separate endpoints (GET and POST) to handle agent chat

--- aiser/ai_server/rest_ai_server.py
+++ aiser/ai_server/rest_ai_server.py
@@ -1,11 +1,23 @@
+import time
+
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from .ai_server import AiServer
+from ..job_management import AsyncStartJobManager, AsyncStartJob
 from ..models.dtos import SemanticSearchRequest, AgentChatRequest, SemanticSearchResultDto, \
     SemanticSearchResultResponseDto, AgentChatResponse, ChatMessageDto
 from ..models import ChatMessage
 import typing
+
+
+class AgentChatJob(AsyncStartJob):
+    def __init__(self, agent_chat_request: AgentChatRequest):
+        super().__init__(creation_timestamp=time.time())
+        self._agent_chat_request = agent_chat_request
+
+    def get_agent_chat_request(self) -> AgentChatRequest:
+        return self._agent_chat_request
 
 
 class RestAiServer(AiServer):
@@ -35,12 +47,27 @@ class RestAiServer(AiServer):
                 message_gen: typing.AsyncGenerator[ChatMessage, None]) -> typing.AsyncGenerator[str, None]:
             async for item in message_gen:
                 message_dto = ChatMessageDto(textContent=item.text_content)
-                yield AgentChatResponse(outputMessage=message_dto).model_dump_json(by_alias=True)
+                yield AgentChatResponse(outputMessage=message_dto).model_dump_json(by_alias=True) + "\n"
 
-        @app.post("/agent/{agent_id}/chat")
-        async def agent_chat(agent_id: str, request: AgentChatRequest) -> StreamingResponse:
+        job_manager = AsyncStartJobManager()
+
+        @app.post("/agent/{agent_id}/chat/{job_id}")
+        async def agent_chat(agent_id: str, job_id: str, request: AgentChatRequest):
             for agent in self._agents:
                 if agent.accepts_id(agent_id):
+                    new_job = AgentChatJob(agent_chat_request=request)
+                    job_manager.define_job(job_id=job_id, job=new_job)
+                    return
+            raise HTTPException(status_code=404, detail="Agent not found")
+
+        @app.get("/agent/{agent_id}/chat/{job_id}")
+        async def agent_chat(agent_id: str, job_id: str) -> StreamingResponse:
+            for agent in self._agents:
+                if agent.accepts_id(agent_id):
+                    job = await job_manager.wait_for_job(job_id=job_id)
+                    if job is None:
+                        raise HTTPException(status_code=404, detail="Job not found")
+                    request = job.get_agent_chat_request()
                     messages = [ChatMessage(text_content=messageDto.textContent) for messageDto in request.messages]
                     response_generator = agent.reply(messages=messages)
                     response_generator = convert_agent_message_gen_to_streaming_response(message_gen=response_generator)
