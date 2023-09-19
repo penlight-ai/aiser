13d72eb74b172e1a43c5bc32b3e39cba8c54622d
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-19 15:07:16 +0300

    Add ability to set environment type
    Add ability to specify 'Production' or 'Development' through an environment variable

--- aiser/config/ai_server_config.py
+++ aiser/config/ai_server_config.py
@@ -4,30 +4,64 @@ import typing
 from pydantic import BaseModel
 
 
+class ServerEnvironment:
+    DEVELOPMENT = 'Development'
+    PRODUCTION = 'Production'
+
+
 class AiServerConfig(BaseModel):
     consumer: AiServerConsumer
     complete_url: str
+    server_environment: str
 
 
 def make_ai_server_config(
         complete_url: str,
         consumer: typing.Optional[AiServerConsumer] = None
 ) -> AiServerConfig:
-    env_variables = get_ai_server_env_variables()
-
-    final_consumer = _determine_consumer(consumer=consumer, env_variables=env_variables)
-
-    return AiServerConfig(
+    config_maker = AiServerConfigMaker(
         complete_url=complete_url,
-        consumer=final_consumer
+        env_variables=get_ai_server_env_variables(),
+        overriding_consumer=consumer
     )
+    return config_maker.make_config()
 
 
-def _determine_consumer(consumer: typing.Optional[AiServerConsumer], env_variables: AiServerEnvVariables):
-    if consumer is not None:
-        return consumer
-    if env_variables.CONSUMER_PUBLIC_KEY_INFO_URL is not None:
-        return AiServerConsumer(
-            publicKeyInfoUrl=env_variables.CONSUMER_PUBLIC_KEY_INFO_URL
+class AiServerConfigMaker:
+    def __init__(
+            self,
+            complete_url: str,
+            env_variables: AiServerEnvVariables,
+            overriding_consumer: typing.Optional[AiServerConsumer] = None,
+    ):
+        self.complete_url = complete_url
+        self.env_variables = env_variables
+        self.overriding_consumer = overriding_consumer
+
+    def make_config(self) -> AiServerConfig:
+        final_consumer = self._determine_consumer()
+        final_server_environment = self._determine_server_environment()
+
+        return AiServerConfig(
+            complete_url=self.complete_url,
+            consumer=final_consumer,
+            server_environment=final_server_environment
         )
-    return get_default_consumer()
+
+    def _determine_consumer(
+            self,
+    ) -> AiServerConsumer:
+        if self.overriding_consumer is not None:
+            return self.overriding_consumer
+        if self.env_variables.CONSUMER_PUBLIC_KEY_INFO_URL is not None:
+            return AiServerConsumer(
+                publicKeyInfoUrl=self.env_variables.CONSUMER_PUBLIC_KEY_INFO_URL
+            )
+        return get_default_consumer()
+
+    def _determine_server_environment(
+            self
+    ) -> str:
+        if self.env_variables.AISER_ENVIRONMENT == ServerEnvironment.DEVELOPMENT:
+            return ServerEnvironment.DEVELOPMENT
+        return ServerEnvironment.PRODUCTION
--- aiser/config/ai_server_env_variables.py
+++ aiser/config/ai_server_env_variables.py
@@ -5,9 +5,11 @@ from pydantic import BaseModel, AnyHttpUrl
 
 class AiServerEnvVariables(BaseModel):
     CONSUMER_PUBLIC_KEY_INFO_URL: typing.Optional[AnyHttpUrl]
+    AISER_ENVIRONMENT: typing.Optional[str]
 
 
 def get_ai_server_env_variables() -> AiServerEnvVariables:
     return AiServerEnvVariables(
-        CONSUMER_PUBLIC_KEY_INFO_URL=os.environ.get("CONSUMER_PUBLIC_KEY_INFO_URL")
+        CONSUMER_PUBLIC_KEY_INFO_URL=os.environ.get("CONSUMER_PUBLIC_KEY_INFO_URL"),
+        AISER_ENVIRONMENT=os.environ.get("AISER_ENVIRONMENT")
     )
