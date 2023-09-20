fa418f3f6a95d3a046937a7f5294700da2765473
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-20 12:32:43 +0300

    Clear the github pages related config for docs

--- docs/aiser/docusaurus.config.js
+++ docs/aiser/docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: '', // Usually your GitHub org/user name.
+    projectName: '', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
