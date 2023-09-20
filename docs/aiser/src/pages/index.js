eab344ecf95264ec67ea2bc41e4e0bc5d77a56b9
zaidimad <89179271+zaidimad@users.noreply.github.com>
2023-09-20 12:21:16 +0300

    Update title and description for docs home page

--- docs/aiser/src/pages/index.js
+++ docs/aiser/src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Aiser Docs`}
+      description="The Official Aiser Documentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
